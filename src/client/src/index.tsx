5b58e0a7a2bba15311836954980b6cb56c25f55b
Vinícius <vinikappke@gmail.com>
2022-04-24 01:30:06 -0300

    Created MUI Theme

--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -1,14 +1,37 @@
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, createTheme, Theme, ThemeProvider } from "@mui/material";
 import React from "react";
 import * as ReactDOM from "react-dom/client";
 import App from "./components/App";
+
+export const theme: Theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#84335D",
+      contrastText: "#E8D9E1",
+    },
+    secondary: {
+      main: "#B6869F",
+    },
+    background: {
+      default: "#1c1016",
+      paper: "#50223A",
+    },
+    divider: "#E8D9E1",
+    text: {
+      primary: "#E8D9E1",
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
