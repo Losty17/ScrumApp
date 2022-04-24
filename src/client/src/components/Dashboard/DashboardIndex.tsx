3f950aefc33985804b94030fc0525c1c1cac2227
Vinícius <vinikappke@gmail.com>
2022-04-24 14:37:51 -0300

    Adding dashboard navigation and sidebar

--- src/client/src/components/Dashboard/DashboardIndex.tsx
+++ src/client/src/components/Dashboard/DashboardIndex.tsx
@@ -1,14 +1,18 @@
 import * as React from "react";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import DashboardSidebar from "./DashboardSidebar";
+import DashboardViews from "./DashboardViews";
+import { Box } from "@mui/material";
 
 interface IDashboardProps {}
 const Dashboard: React.FC<IDashboardProps> = () => {
-  const navigate = useNavigate();
+  const [view, setView] = useState("overview");
 
   /**
    * Validate session
    */
+  const navigate = useNavigate();
   useEffect(() => {
     const token = localStorage.getItem("token") || "";
     fetch("/api/authenticate", { headers: { token: token } }).then(
@@ -18,11 +22,13 @@ const Dashboard: React.FC<IDashboardProps> = () => {
         if (!data.auth) navigate("/login");
       }
     );
-  }, []);
+  }, [navigate]);
+
   return (
-    <>
-      <h1>Dashboard</h1>
-    </>
+    <Box sx={{ display: "flex" }}>
+      <DashboardSidebar view={view} setView={setView} />
+      <DashboardViews route={view} />
+    </Box>
   );
 };
 
